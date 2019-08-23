3f093348092feb35d4a74fc53e61a8fdf6351e23
João Phellipe de Freitas Pinto <phellipe@ufersa.edu.br>
2019-08-23 11:03:52 -0300

    added multer, avatarcontroller, and other features

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,4 +17,7 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 export default routes;
