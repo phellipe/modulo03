c65480b64ea16fd39bd40da26c82193765789337
João Phellipe de Freitas Pinto <phellipe@ufersa.edu.br>
2019-08-23 16:03:27 -0300

    added schedules, appointments

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -13,6 +15,9 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+/**
+ * From now on the user MUST be authenticated to access the routes
+ */
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
@@ -20,4 +25,8 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.get('/schedules', ScheduleController.index);
+
 export default routes;
